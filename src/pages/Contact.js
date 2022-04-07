d420e3b1095264b73141153bd838814c14e87003
David Redding <redding.david35@gmail.com>
2022-04-07 13:39:01 -0500

    pages init

---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum sed dolor, maiores similique repudiandae id
+      quod esse cumque voluptates, aliquam autem facere, omnis laborum aspernatur deserunt animi iste molestias?
+    </p>
+  </div>
+);
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => (
+  <div>
+    <h2>Homepage</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum sed dolor, maiores similique repudiandae id
+      quod esse cumque voluptates, aliquam autem facere, omnis laborum aspernatur deserunt animi iste molestias?
+    </p>
+  </div>
+);
+
+export default Home;
