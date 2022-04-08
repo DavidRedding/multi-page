a74ce23c654c70fd7b220492c44fe03cfac9e9f1
David Redding <redding.david35@gmail.com>
2022-04-08 17:55:18 -0500

    URLSearchParams implemented & tested

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,11 +2,12 @@ import { useLocation } from 'react-router-dom';
 
 const Contact = () => {
   const queryString = useLocation().search;
-  console.log(queryString);
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get('name'); // add ?name=mario as a query parameter to see this work.
 
   return (
     <div className=" space-y-2 mb-8">
-      <h2 className="font-semibold text-lg">Contact</h2>
+      <h2 className="font-semibold text-lg">Hey {name}, Contact us here...</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum sed dolor, maiores similique repudiandae id
         quod esse cumque voluptates, aliquam autem facere, omnis laborum aspernatur deserunt animi iste molestias?
